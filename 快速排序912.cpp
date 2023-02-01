90eecd7cdae825832a2ae3a206ec0d0d1cbcb5af
Fabio <88869510+CrystalParaboy@users.noreply.github.com>
2023-02-01 15:50:42 +0800

    Update 快速排序912.cpp

--- 快速排序912.cpp
+++ 快速排序912.cpp
@@ -1,5 +1,37 @@
 class Solution {
 public:
+     void maxHeap(vector<int>& nums, int i, int len){
+        while( (i<<1)+1<=len ){
+            int lson = (i<<1) + 1;
+            int rson = (i<<1) + 2;
+            int large = i;
+            if(lson <= len && nums[lson] > nums[large])
+                large = lson;
+            if(rson <= len && nums[rson] > nums[large])
+                large = rson;
+            if(large != i ){
+                swap(nums[large], nums[i]);
+                i =  large;
+            }
+            else break;
+        }
+    }
+    void heapSort(vector<int>&nums){
+        //build heap
+        int len = nums.size()-1;
+        for(int i =  len/2; i>=0; --i){
+            maxHeap(nums, i, len);
+        }
+
+        //sort
+        while(len >= 0){
+            swap(nums[0], nums[len]);
+            --len;
+            maxHeap(nums, 0, len);
+        }
+        return;
+    }
+	
     void quickSort(vector<int> &nums, int l, int r){
         if(l<r){
 			//swapping element at index l with element at random index
